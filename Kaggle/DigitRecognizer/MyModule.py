c335add61b5b877810ede9260977ad8e3d43eba7
MiEcoku <45246904+MiEcoku@users.noreply.github.com>
2025-04-17 21:00:52 +0800

    Update MyModule.py

--- Kaggle/DigitRecognizer/MyModule.py
+++ Kaggle/DigitRecognizer/MyModule.py
@@ -18,3 +18,24 @@ class CNN(torch.nn.Module):
         x = torch.flatten(x, start_dim = 1)
         x = self.liner(x)
         return x
+
+class LeNet(torch.nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels = 1, out_channels = 6, kernel_size = 5, padding = 2),
+            torch.nn.Sigmoid(),
+            torch.nn.MaxPool2d(kernel_size = 2, stride = 2),
+            torch.nn.Conv2d(in_channels = 6, out_channels = 16, kernel_size = 5),
+            torch.nn.Sigmoid(),
+            torch.nn.MaxPool2d(kernel_size = 2, stride = 2),
+            torch.nn.Flatten(),
+            torch.nn.Linear(in_features = 16 * 5 * 5, out_features = 120),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(in_features = 120, out_features = 84),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(in_features = 84, out_features = 10)
+        )
+    
+    def forward(self, x):
+        return self.net(x)
